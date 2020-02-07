b3f25b19449d599e9c6f90c9b89a1005159e32e4
ARMANI1294 <arielmacmani1294@gmail.com>
2020-02-07 07:50:09 -0600

    Repositorio nvo

---
+++ welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Proyecto 1</h1>
+    <table style="width:100%"  class="btn btn-success">
+        <tr>
+            <p>Ya vamonos</p>
+            <td style="border: solid 1px black; width:50%">si</td>
+            <td style="border: solid 1px black; width:50%">No</td>
+        </tr>
+    </table>
+</body>
+</html>
